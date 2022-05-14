ddf838cf35649c57b40c9f965788e8ec38143f45
Parker Ottaway <pottaway@asu.edu>
2022-05-14 08:30:53 -0600

    feat: day2_part1 src, tests, input data, and README
    Also fixed the git lfs and will now track all .txt files in lfs.

---
+++ src/day02/README.md
@@ -0,0 +1,21 @@
+# Day 02
+
+## Part 1
+
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a
+list of the dimensions (length l, width w, and height h) of each present, and only want to order
+exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating
+the required wrapping paper for each gift a little easier: find the surface area of the box,
+which is ![equation](https://latex.codecogs.com/svg.image?\bg{white}2&space;\times&space;l&space;\times&space;w&space;&plus;&space;2&space;\times&space;w&space;\times&space;h&space;&plus;&space;2&space;\times&space;h&space;\times&space;l).
+The elves also need a little extra paper for each present: the
+area of the smallest side.
+
+For example:
+
+A present with dimensions 2x3x4 requires ![equation](https://latex.codecogs.com/svg.image?\bg{white}2&space;\times&space;6&space;&plus;&space;2&space;\times12&space;&plus;&space;2&space;\times&space;8&space;=&space;52&space;\textrm{&space;ft}^2)
+of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+A present with dimensions 1x1x10 requires ![equation](https://latex.codecogs.com/svg.image?\bg{white}2&space;\times&space;1&space;&plus;&space;2&space;\times&space;10&space;&plus;&space;2&space;\times&space;10&space;=&space;42&space;\textrm{&space;ft}^2)
+of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
---
+++ src/day02/part1.rs
@@ -0,0 +1,103 @@
+use std::{env, fs};
+
+fn surface_area_of_box(length: u32, width: u32, height: u32) -> u32 {
+    return 2 * (length * width + width * height + height * length);
+}
+
+fn build_sorted_dimension_vec(s: &str) -> Vec<u32> {
+    let mut split_dims: Vec<u32> = s
+        .split("x")
+        .filter_map(|e| match e.parse::<u32>().unwrap() {
+            0u32 => panic!("Encountered a zero dimension!"),
+            _ => Some(e.parse::<u32>().unwrap()),
+        })
+        .collect::<Vec<u32>>();
+    split_dims.sort();
+    return split_dims;
+}
+
+/// Total surface area plus the area of the smallest side.
+fn calculate_single_present_surface_area_needed(s: &str) -> u32 {
+    let sorted_dim: Vec<u32> = build_sorted_dimension_vec(s);
+    // Area of smallest side is the product of the two smallest dimensions.
+    return surface_area_of_box(sorted_dim[0], sorted_dim[1], sorted_dim[2])
+        + sorted_dim[0] * sorted_dim[1];
+}
+
+fn calculate_total_wrapping_paper_needed(sv_vec: &Vec<&str>) -> u32 {
+    let mut total_paper_needed: u32 = 0u32;
+    sv_vec
+        .iter()
+        .for_each(|e| total_paper_needed += calculate_single_present_surface_area_needed(e));
+    return total_paper_needed;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 1 {
+        panic!(
+            "Expected one argument. Got {}.\n\nUsage: ./day02_part1 src/day02/data/part1_input.txt",
+            args.len()
+        );
+    }
+    let input_file_path: &str = args.first().unwrap();
+    let file_content: String = fs::read_to_string(input_file_path).unwrap();
+
+    let split_file_content: Vec<&str> = file_content.split("\n").collect();
+    println!(
+        "Total wrapping paper needed: {} square feet",
+        calculate_total_wrapping_paper_needed(&split_file_content)
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn sa_example_calc() {
+        assert_eq!(surface_area_of_box(2, 3, 4), 52);
+        assert_eq!(surface_area_of_box(1, 1, 10), 42);
+    }
+
+    #[test]
+    fn dimension_vecs_sorted() {
+        assert_eq!(build_sorted_dimension_vec("2x2x2"), [2, 2, 2]);
+        assert_eq!(build_sorted_dimension_vec("3x2x1"), [1, 2, 3]);
+        assert_eq!(build_sorted_dimension_vec("7x8x9"), [7, 8, 9]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_on_negative_dim() {
+        _ = build_sorted_dimension_vec("1x-2x3");
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_on_zero_dim() {
+        _ = build_sorted_dimension_vec("99x99x0");
+    }
+
+    #[test]
+    fn needed_wrapping_paper_example_calcs() {
+        assert_eq!(calculate_single_present_surface_area_needed("2x3x4"), 58);
+        assert_eq!(calculate_single_present_surface_area_needed("1x1x10"), 43);
+    }
+
+    #[test]
+    fn calculate_required_paper_from_dims_list() {
+        let v: Vec<&str> = vec!["2x3x4", "1x1x10"];
+        assert_eq!(calculate_total_wrapping_paper_needed(&v), 58 + 43)
+    }
+
+    #[test]
+    fn check_challenge_input() {
+        let file_content: String = fs::read_to_string("src/day02/data/part1_input.txt").unwrap();
+
+        let split_file_content: Vec<&str> = file_content.split("\n").collect();
+        assert_eq!(
+            calculate_total_wrapping_paper_needed(&split_file_content),
+            1598415
+        );
+    }
+}
